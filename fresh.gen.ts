a49b252aca2796531535164258841377530dbead
SnowCait <SnowCait@users.noreply.github.com>
2024-04-15 14:10:13 +0900

    Delete unused code

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,10 +5,7 @@
 import * as $_pair_date_ from "./routes/[pair]/[date].ts";
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
-import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
+
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -16,13 +13,8 @@ const manifest = {
     "./routes/[pair]/[date].ts": $_pair_date_,
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
-    "./routes/index.tsx": $index,
   },
-  islands: {
-    "./islands/Counter.tsx": $Counter,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
